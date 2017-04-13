bc87fd1d921ae2e82de3cf57d620f121108be0d9
rikumax25 <jorgev259@outlook.es>
2017-04-13 08:19:00 -0600

    fixed soundhax not being added (Thanks @saibotu)

--- scripts/step_methods.js
+++ scripts/step_methods.js
@@ -109,7 +109,7 @@ function soundhax_hb(){
     addFile("Otherapp Payload","","otherapp.bin","list");
     
     getFileBuffer_url("https://raw.githubusercontent.com/nedwill/soundhax/master/soundhax-" + region + "-" + console + ".m4a", "Soundhax " + region);
-    addFile("Soundhax","","soundhax-" + region + "-" + console + ".m4a","list");
+    addFile("Soundhax " + region,"","soundhax-" + region + "-" + console + ".m4a","list");
     
     getFileBuffer_url("https://smealum.github.io/ninjhax2/starter.zip","Starter Homebrew Kit");
     extractZip("Starter Homebrew Kit","","starter");
