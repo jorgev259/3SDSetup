11fe6d528fb8e6517ad0e123def9bae427b50601
rikumax25 <jorgev259@outlook.es>
2017-07-03 12:32:57 -0600

    Edge compatibility

--- scripts/methods.js
+++ scripts/methods.js
@@ -25,7 +25,11 @@
                 try{
                     download_msg.find(".toast-message").text( "Once all downloads finish, click 'Download Zip' and extract everything inside " + zip_name + ".zip into your SD Card");
                 }finally{
-                    saveAs(blob, zip_name + ".zip");
+                    if(window.navigator.userAgent.indexOf("Edge") > -1){
+                        window.navigator.msSaveBlob(blob, zip_name + ".zip");
+                    }else{
+                        saveAs(blob, zip_name + ".zip");
+                    }
                 }
             });
         })
